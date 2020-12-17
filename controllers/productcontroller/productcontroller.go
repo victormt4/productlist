36a329269e8c3eec0733db65559669f2e7deb592
Victor Mota <victor.vmt@outlook.com>
2020-12-17 10:43:49 -0300

    Criando controller para adicionar produto

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -3,7 +3,10 @@ package productcontroller
 import (
 	"html/template"
 	"net/http"
+	"productlist/model"
 	"productlist/repo/productrepo"
+	"productlist/services/responseservice"
+	"strconv"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
@@ -13,4 +16,38 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	products := productrepo.GetAll()
 
 	temp.ExecuteTemplate(w, "Index", products)
+}
+
+func Add(w http.ResponseWriter, r *http.Request) {
+
+	resService := responseservice.GetResponseServiceWriter(w)
+
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		description := r.FormValue("description")
+
+		price, err := strconv.ParseFloat(r.FormValue("price"), 64)
+		if err != nil {
+			price = 0.0
+		}
+
+		quantity, err := strconv.Atoi(r.FormValue("quantity"))
+		if err != nil {
+			quantity = 0
+		}
+
+		product := productrepo.Insert(model.Product{
+			Name:        name,
+			Description: description,
+			Price:       price,
+			Quantity:    quantity,
+		})
+
+		resService.SendSuccess(responseservice.DefaultResponseStructure{
+			Message: "Product successfully registered", Data: product,
+		})
+
+	} else {
+		resService.SendNotAllowed(nil)
+	}
 }
